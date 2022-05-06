7172f960c8621c645ca5f189dfd2bea7db4ea1ad
Christopher Bladwell <c.bladwell@unsw.edu.au>
2022-05-06 14:12:22 +1000

    delete write statements

--- src/shared/diag_manager/diag_manager.F90
+++ src/shared/diag_manager/diag_manager.F90
@@ -601,8 +601,7 @@ CONTAINS
 
           ! check the initial time - DELETE
           CALL get_time(init_time,second_init,day_init)
-          write(*,*) '! initial time:', second_init+day_init*24*60*60
-
+          
           ! Get output frequency from for the appropriate output file
           file_num = output_fields(ind)%output_file
           IF ( file_num == max_files ) CYCLE
@@ -1720,13 +1719,6 @@ CONTAINS
             CALL get_time(output_fields(out_num)%next_next_output,second_next_next,day_next_next)
             weight1 = (second_next_next+day_next_next*60.0*60.0*24.0)-(second+day*60.0*60.0*24.0) !- ts/2.0
           END IF
-          ! CHECKS FOR TIME OUTPUTS - DELETE THESE
-          ! CALL get_time(output_fields(out_num)%last_output,second_previous,day_previous)
-          write(*,*) '! 1d count:', count
-          write(*,*) '! 1e time step:', ts, 'count: ', count,'field: ', diag_name
-          write(*,*) '! 1f elapsed time:', ((second+day*60.0*60.0*24.0)-(second_previous+day_previous*60.0*60.0*24.0))
-          write(*,*) '! 1 next-current=weight', weight1
-          write(*,*) '! next time', second_next+day_next*60.0*60.0*24.0
        END IF
 
        ! compute the diurnal index
